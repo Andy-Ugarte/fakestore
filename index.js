c0cd8c4f2e692e6748bcce1b2d60381a5bb03e46
Andy-Ugarte <andy.ugarte.schirrmeister@gmail.com>
2023-11-05 17:40:40 -0700

    cards and fetch

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let url = 'https://fakestoreapi.com/products'
 let electronics = document.getElementById('electronics')
     //console.log(electronics)
 
-let jewelery = document.getElementById('jewelery')  /// check spelling
+let jewelery = document.getElementById('jewelery')  /// check spelling (how to connect this to nav bar?????)
 
 let men = document.getElementById('men')
 
@@ -18,9 +18,24 @@ const fakeStore = async(endpoint) => { //endpoint pass function
     let results = await res.json()
     console.log(results)
 }
+window.onload = (e) => {
+    getFakeStoreData
+}
+
 
 //fetch - await
-
+fetch(url)
+    .then(response => response.json())
+    .then(data=>{
+        console.log(data);
+        let fakeStore;
+        // create variable AND create new element
+        fakeStore = document.createElement('card')
+        // change the text of our card element??????
+        fakeStore.innerText = data;
+        // append the child on the webpage
+        document.body.appendChild(fakeStore);
+    })
 
 
 electronics.addEventListener('click', () =>{
@@ -29,7 +44,19 @@ electronics.addEventListener('click', () =>{
     fakeStore('/category/electronics')
 })
 
+// Endpoint for jewelery
+electronics.addEventListener('click', () =>{
+    //call back function it => {}. inside {} is what happends when button is clicked
+    // Call fakeStore with endpoint as argument /categoty/electronics
+    fakeStore('/category/electronics')
+})
 
 // Endpoint for men and women is lower caser /category/men's clothing
+men.addEventListener('click', () => {
+    fakeStore('/catergory/men')
+})
 
 // Endpoint for men and women is lower caser /category/women's clothing
+women.addEventListener('click', () =>{
+    fakeStore('/catergory/women')
+})
