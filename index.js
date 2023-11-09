bdbecf0db8c166c7e6762abd25d7b06bb0c57576
Andy-Ugarte <andy.ugarte.schirrmeister@gmail.com>
2023-11-09 15:31:20 -0700

    Anothony help

--- index.js
+++ index.js
@@ -1,62 +1,137 @@
 let url = 'https://fakestoreapi.com/products'
-
 let electronics = document.getElementById('electronics')
     //console.log(electronics)
-
 let jewelery = document.getElementById('jewelery')  /// check spelling (how to connect this to nav bar?????)
-
 let men = document.getElementById('men')
-
 let women = document.getElementById('women')
-
 let display = document.getElementById('display')
+let cart = {}
+
 
 const fakeStore = async(endpoint) => { //endpoint pass function
     // body of async arrow function
     let res = await fetch(url+endpoint)
-    console.log(res)
     let results = await res.json()
-    console.log(results)
-}
-window.onload = (e) => {
-    getFakeStoreData
+   displayCards(results)
 }
 
+function displayCards (products){
+   // console.log(products)  
+    // make a for each loop for products. products.foreachproduct / 
+   //while loop
+    while(display.firstChild){
+        display.removeChild(display.firstChild)
+    }
+    products.forEach((product) => {
+        console.log(product)
 
-//fetch - await
-fetch(url)
-    .then(response => response.json())
-    .then(data=>{
-        console.log(data);
-        let fakeStore;
-        // create variable AND create new element
-        fakeStore = document.createElement('card')
-        // change the text of our card element??????
-        fakeStore.innerText = data;
-        // append the child on the webpage
-        document.body.appendChild(fakeStore);
+        let card = document.createElement('div');
+        let image = document.createElement('img');
+        let cardBody = document.createElement('div');
+        let cardTitle = document.createElement('h5')
+        let cardText = document.createElement('p')
+        let addToCartBtn = document.createElement ('a')
+   
+        card.className = 'card'
+        card.style = 'width: 18rem'
+        image.className = 'card-img-top'
+        cardBody.className = 'card-body'
+        cardTitle.className = 'card-title'
+        cardText.className = 'card-text'
+        addToCartBtn.className = 'btn btn-primary'
+        
+        cardTitle.innerText = product.title
+        cardText.innerText = product.description
+        image.src = product.image
+        addToCartBtn.innerText = 'add to cart'
+        
+        addToCartBtn.onclick = function(){
+           const item = {
+            id: product.id,
+            title: product.title,
+            cost: '$'+ product.price.toFixed(2),
+            quantity: 1
+           }
+           //console.log(item);
+           submitToCart(item);
+        };
+        
+        display.appendChild(card);
+        card.appendChild(image);
+        card.appendChild(cardBody)
+        cardBody.appendChild(cardTitle);
+        cardBody.appendChild(cardText);
+        cardBody.appendChild(addToCartBtn)
+        
+        
     })
 
+}
 
-electronics.addEventListener('click', () =>{
-    //call back function it => {}. inside {} is what happends when button is clicked
-    // Call fakeStore with endpoint as argument /categoty/electronics
-    fakeStore('/category/electronics')
-})
+function submitToCart(item) {
+    console.log(cart)
+    // console.log(item)
+    if (cart.length > 0){
+        console.log(cart.length);
+        cart.forEach((item) => {
+            if (cart.includes(item.id)){
+                item.quantity= item.quantity+1
+                console.log('hello')
+            }   else {
+                cart.push('new item in for each',item)
+            }
+        })
+    }   else {
+        cart.push('new item out for each', item)
+    }
 
-// Endpoint for jewelery
-electronics.addEventListener('click', () =>{
-    //call back function it => {}. inside {} is what happends when button is clicked
-    // Call fakeStore with endpoint as argument /categoty/electronics
-    fakeStore('/category/electronics')
-})
+    console.log(cart)
+        //let item = []
+    //console.log('submit to cart', item)
+    //cart.push(item)
+    //console.log(cart)
+    //console.log('submit to cart', item.id)
+    
+    // cardTitle.innerText = product.title
+    // cardText.innerText = product.description
+    // image.src = product.image
+    // addToCartBtn.innerText = 'add to cart'
+    
 
-// Endpoint for men and women is lower caser /category/men's clothing
-men.addEventListener('click', () => {
-    fakeStore('/catergory/men')
-})
 
-// Endpoint for men and women is lower caser /category/women's clothing
-women.addEventListener('click', () =>{
-    fakeStore('/catergory/women')
-})
+}
+
+    
+    electronics.addEventListener('click', () =>{
+        //call back function it => {}. inside {} is what happends when button is clicked
+        // Call fakeStore with endpoint as argument /category/electronics
+        fakeStore('/category/electronics')
+    })
+    
+    // Endpoint for jewelery
+    jewelery.addEventListener('click', () =>{
+        //call back function it => {}. inside {} is what happends when button is clicked
+        // Call fakeStore with endpoint as argument /category/electronics
+        fakeStore('/category/jewelery')
+    })
+    
+    // Endpoint for men and women is lower caser /category/men's clothing
+    men.addEventListener('click', () => {
+        fakeStore(`/category/men's clothing`)
+    })
+    
+    // Endpoint for men and women is lower caser /category/women's clothing
+    women.addEventListener('click', () =>{
+        fakeStore(`/category/women's clothing`)
+    })
+    
+    
+     window.onload = (e) => {
+         fakeStore('/')
+     }
+
+     category.addEventListener('click', (e)=> {
+        e.preventDefault();
+        console.log(e.target.search.value)
+        category(e.target)
+     })
